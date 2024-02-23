e931757d234887fd21de0728cd777675df82ea0e
dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-02-23 11:12:16 -0600

    Controllers actulizado

--- controllers/alumno.controller.js
+++ controllers/alumno.controller.js
@@ -2,15 +2,15 @@ const bcryptjs = require('bcryptjs');
 const Alumno = require('../models/alumno');
 const { response } = require('express');
 
-const alumnoGet = async (req, res = response) => {
-    const { limite, desde } = req.query;
-    const query = { estado: true };
+const alumnoGet = async  (req, res = response) => {
+    const {limite, desde} = req.query;
+    const query = {estado: true};
 
     const [total, alumnos] = await Promise.all([
         Alumno.countDocuments(query),
         Alumno.find(query)
-            .skip(Number(desde))
-            .limit(Number(limite))
+        .skip(Number(desde))
+        .limit(Number(limite))
     ]);
 
     res.status(200).json({
@@ -19,9 +19,9 @@ const alumnoGet = async (req, res = response) => {
     })
 }
 
-const getAlumnoById = async (req, res) => {
-    const { id } = req.params;
-    const alumno = await Alumno.findOne({ _id: id });
+const getAlumnoById = async (req, res) =>{
+    const {id} = req.params;
+    const alumno = await Alumno.findOne({_id : id});
 
     res.status(200).json({
         alumno
@@ -47,9 +47,9 @@ const putAlumno = async (req, res = response) => {
     });
 }
 
-const alumnoDelete = async (req, res) => {
-    const { id } = req.params;
-    const alumno = await Alumno.findByIdAndUpdate(id, { estado: false });
+const alumnoDelete = async (req, res) =>{
+    const {id} = req.params;
+    const alumno = await Alumno.findByIdAndUpdate(id, {estado: false}); 
     const alumnoAutenticado = req.alumno;
 
     res.status(200).json({
@@ -61,9 +61,9 @@ const alumnoDelete = async (req, res) => {
 
 
 
-const alumnoPost = async (req, res) => {
-    const { nombre, correo, password, role, curso } = req.body;
-    const alumno = new Alumno({ nombre, correo, password, role, curso });
+const alumnoPost = async (req, res) =>{
+    const {nombre, correo, password, role, curso} = req.body;
+    const alumno = new Alumno({nombre, correo, password, role, curso});
 
     const salt = bcryptjs.genSaltSync();
     alumno.password = bcryptjs.hashSync(password, salt);
